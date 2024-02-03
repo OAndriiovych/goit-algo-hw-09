ef4456c3ba11c9c2e03783e7b6c61be7c6ad31de
narnanam <narnanam@gmail.com>
2024-02-03 21:58:24 +0200

    init

---
+++ rest.py
@@ -0,0 +1,31 @@
+def find_coins_greedy(target_rest, coins=[50, 25, 10, 5, 2, 1]):
+    result = {}
+    for coin in coins:
+        count = target_rest // coin
+        if count >= 1:
+            target_rest = target_rest - count * coin
+            result[coin] = count
+    if target_rest != 0:
+        raise Exception("you have no pennies to spend the rest")
+    return result
+
+
+def find_min_coins(target_rest, coins=[50, 25, 10, 5, 2, 1]):
+    matrix = [[0 for _ in range(target_rest + 1)] for _ in range(len(coins) + 1)]
+    result = {}
+    for i in range(len(coins) + 1):
+        for j in range(target_rest + 1):
+            if i == 0 or j == 0:
+                continue
+            coin = coins[i - 1]
+            previous_best_count = matrix[i][j - 1]
+            count = target_rest // coin
+            if count >= 1:
+                target_rest = target_rest - count * coin
+                result[coin] = count
+            matrix[i][j] = previous_best_count + count
+    return result
+
+
+print(find_coins_greedy(134))
+print(find_min_coins(23))
